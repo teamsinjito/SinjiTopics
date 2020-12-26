c0bd21e3a735170149a2dbb2114a969035475ffd
nanaru <teamsinjito.nanaru@gmail.com>
2020-12-26 10:46:55 +0900

    20201226_アニメーションスピード調整3

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -2500,32 +2500,31 @@ __webpack_require__.r(__webpack_exports__);
   computed: {
     endRollSpeed: function endRollSpeed() {
       var ln = this.text.length;
-      var ani = ""; // var sec = Math.floor(ln / 10)   
-      // ani = "EndRoll " + String(sec) + "s infinite"
-
-      if (ln >= 2000) {
-        ani = "EndRoll 120s infinite";
-      } else if (ln >= 1800) {
-        ani = "EndRoll 100s infinite";
-      } else if (ln >= 1400) {
-        ani = "EndRoll 100s infinite";
-      } else if (ln >= 1200) {
-        ani = "EndRoll 90s infinite";
-      } else if (ln >= 1000) {
-        ani = "EndRoll 80s infinite";
-      } else if (ln >= 800) {
-        ani = "EndRoll 80s infinite";
-      } else if (ln >= 600) {
-        ani = "EndRoll 70s infinite";
-      } else if (ln >= 400) {
-        ani = "EndRoll 70s infinite";
-      } else if (ln >= 200) {
-        ani = "EndRoll 60s infinite";
-      } else if (ln >= 100) {
-        ani = "EndRoll 60s infinite";
-      } else {
-        ani = "EndRoll 50s infinite";
-      }
+      var ani = "";
+      var sec = Math.floor(ln / 10);
+      ani = "EndRoll " + String(sec) + "s infinite"; // if(ln>=2000){
+      //     ani ="EndRoll 120s infinite"
+      // }else if(ln>=1800){
+      //     ani ="EndRoll 100s infinite"
+      // }else if(ln>=1400){  
+      //     ani ="EndRoll 100s infinite"
+      // }else if(ln>=1200){
+      //     ani="EndRoll 90s infinite"
+      // }else if(ln>=1000){
+      //     ani="EndRoll 80s infinite"
+      // }else if(ln>=800){
+      //     ani="EndRoll 80s infinite"
+      // }else if(ln>=600){
+      //     ani="EndRoll 70s infinite"
+      // }else if(ln>=400){
+      //     ani="EndRoll 70s infinite"
+      // }else if(ln>=200){
+      //     ani="EndRoll 60s infinite"
+      // }else if(ln>=100){
+      //     ani="EndRoll 60s infinite"
+      // }else{
+      //     ani="EndRoll 50s infinite"
+      // }
 
       return {
         animation: ani,
--- src/resources/js/components/MovieEndroll.vue
+++ src/resources/js/components/MovieEndroll.vue
@@ -16,31 +16,31 @@ export default {
         endRollSpeed(){
             var ln = this.text.length
             var ani = ""
-            // var sec = Math.floor(ln / 10)   
-            // ani = "EndRoll " + String(sec) + "s infinite"
-            if(ln>=2000){
-                ani ="EndRoll 120s infinite"
-            }else if(ln>=1800){
-                ani ="EndRoll 100s infinite"
-            }else if(ln>=1400){  
-                ani ="EndRoll 100s infinite"
-            }else if(ln>=1200){
-                ani="EndRoll 90s infinite"
-            }else if(ln>=1000){
-                ani="EndRoll 80s infinite"
-            }else if(ln>=800){
-                ani="EndRoll 80s infinite"
-            }else if(ln>=600){
-                ani="EndRoll 70s infinite"
-            }else if(ln>=400){
-                ani="EndRoll 70s infinite"
-            }else if(ln>=200){
-                ani="EndRoll 60s infinite"
-            }else if(ln>=100){
-                ani="EndRoll 60s infinite"
-            }else{
-                ani="EndRoll 50s infinite"
-            }
+            var sec = Math.floor(ln / 10)   
+            ani = "EndRoll " + String(sec) + "s infinite"
+            // if(ln>=2000){
+            //     ani ="EndRoll 120s infinite"
+            // }else if(ln>=1800){
+            //     ani ="EndRoll 100s infinite"
+            // }else if(ln>=1400){  
+            //     ani ="EndRoll 100s infinite"
+            // }else if(ln>=1200){
+            //     ani="EndRoll 90s infinite"
+            // }else if(ln>=1000){
+            //     ani="EndRoll 80s infinite"
+            // }else if(ln>=800){
+            //     ani="EndRoll 80s infinite"
+            // }else if(ln>=600){
+            //     ani="EndRoll 70s infinite"
+            // }else if(ln>=400){
+            //     ani="EndRoll 70s infinite"
+            // }else if(ln>=200){
+            //     ani="EndRoll 60s infinite"
+            // }else if(ln>=100){
+            //     ani="EndRoll 60s infinite"
+            // }else{
+            //     ani="EndRoll 50s infinite"
+            // }
             return{
                 animation: ani,
                 animationTimingFunction: "linear"
